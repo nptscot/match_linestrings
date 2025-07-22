d1698764e07544e6bc73d8ec2be5feb2978af02e
Dustin Carlino <dabreegster@gmail.com>
2025-07-22 17:18:29 +0100

    Style settings as a card

--- web/src/Settings.svelte
+++ web/src/Settings.svelte
@@ -3,65 +3,69 @@
   export let onChange: () => void;
 </script>
 
-<h3>Settings</h3>
+<div class="card">
+  <div class="card-body">
+    <h3 class="card-title">Settings</h3>
 
-<form>
-  <label class="form-label">
-    Expand the bounding box around each target by this many meters in all
-    directions
+    <form>
+      <label class="form-label">
+        Expand the bounding box around each target by this many meters in all
+        directions
 
-    <input
-      class="form-control"
-      type="number"
-      bind:value={options.buffer_meters}
-      min="0"
-      max="500"
-      step="1"
-      on:change={onChange}
-    />
-  </label>
+        <input
+          class="form-control"
+          type="number"
+          bind:value={options.buffer_meters}
+          min="0"
+          max="500"
+          step="1"
+          on:change={onChange}
+        />
+      </label>
 
-  <label class="form-label">
-    How many degrees difference allowed between matches? (LineStrings pointing
-    in opposite directions are ignored)
+      <label class="form-label">
+        How many degrees difference allowed between matches? (LineStrings
+        pointing in opposite directions are ignored)
 
-    <input
-      class="form-control"
-      type="number"
-      bind:value={options.angle_diff_threshold}
-      min="0"
-      max="90"
-      step="1"
-      on:change={onChange}
-    />
-  </label>
+        <input
+          class="form-control"
+          type="number"
+          bind:value={options.angle_diff_threshold}
+          min="0"
+          max="90"
+          step="1"
+          on:change={onChange}
+        />
+      </label>
 
-  <label class="form-label">
-    How large may the ratio of lengths between the candidates be? (The ratio is
-    always >= 1 (longer.length / shorter.length))
+      <label class="form-label">
+        How large may the ratio of lengths between the candidates be? (The ratio
+        is always >= 1 (longer.length / shorter.length))
 
-    <input
-      class="form-control"
-      type="number"
-      bind:value={options.length_ratio_threshold}
-      min="1"
-      max="10"
-      step="0.1"
-      on:change={onChange}
-    />
-  </label>
+        <input
+          class="form-control"
+          type="number"
+          bind:value={options.length_ratio_threshold}
+          min="1"
+          max="10"
+          step="0.1"
+          on:change={onChange}
+        />
+      </label>
 
-  <label class="form-label">
-    How far away can the midpoints of the candidates be, in meters?
+      <label class="form-label">
+        How far away can the midpoints of the candidates be, in meters?
 
-    <input
-      class="form-control"
-      type="number"
-      bind:value={options.midpt_dist_threshold}
-      min="1"
-      max="50"
-      step="1"
-      on:change={onChange}
-    />
-  </label>
-</form>
+        <input
+          class="form-control"
+          type="number"
+          bind:value={options.midpt_dist_threshold}
+          min="1"
+          max="50"
+          step="1"
+          on:change={onChange}
+        />
+      </label>
+    </form>
+  </div>
+</div>
